86a6697f4e8b148b889c63bfd0bbfafd1e1e43b8
fluffylaw <lydia.cwang@hotmail.com>
2016-09-25 18:21:39 -0400

    Create bluesclues.py

---
+++ asst/1/bluesclues.py
@@ -0,0 +1,14 @@
+# Author of this file: Lydia Wang!
+
+file = open("states.txt", "r")
+d={}
+for line in file:
+    x = line.split(",")
+    a=x[0]
+    b=x[1]
+    c=len(b)-1
+    b=b[0:c]
+    d[a]=b
+    
+def bluesclues(string):
+    print d[string]
